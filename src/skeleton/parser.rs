e603f899b59fa1417b7a045cf095850c71405242
MegaBluejay <dmoiseev2011@gmail.com>
2026-05-03 15:31:12 -0400

    Convert parser/lexer skeleton templates to quote and remove template files

--- src/skeleton/parser.rs
+++
@@ -1,8 +0,0 @@
-use super::lexer::{Token, tokenize};
-
-// TODO: change if codespan_reporting is not used
-use codespan_reporting::diagnostic::Label;
-pub type Diagnostic = codespan_reporting::diagnostic::Diagnostic<()>;
-
-include!(concat!(env!("OUT_DIR"), "/generated.rs"));
-
