2a86304c824b65eb7bd968ba2e468fc8331e5f68
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2026-08-22 11:24:14 +0200

    Update generated parser

--- src/frontend/generated.rs
+++ src/frontend/generated.rs
@@ -705,7 +705,7 @@ impl<'a> Parser<'a> {
         diags: &mut Vec<<Self as ParserCallbacks<'a>>::Diagnostic>,
     ) -> Parser<'a>
     where
-        <Self as ParserCallbacks<'a>>::Context: Default,
+        for<'trivial_bound> <Self as ParserCallbacks<'a>>::Context: Default,
     {
         #[allow(clippy::unit_arg)]
         Self::new_with_context(
